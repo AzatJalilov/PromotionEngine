cdc5fb16aa155cbff59f6332c6229f036361d835
azat.jalilov <azat.jalilov@check24.de>
2021-02-17 11:27:49 +0100

    Add amount calculator tests

---
+++ PromotionEngine.Tests/AmountCalculatorTests.cs
@@ -0,0 +1,34 @@
+﻿namespace PromotionEngine.Tests
+{
+    using System.Collections.Generic;
+    using FluentAssertions;
+    using PromotionEngine.Domain;
+    using Xunit;
+
+    public class AmountCalculatorTests
+    {
+        private readonly AmountCalculator sut;
+
+        public AmountCalculatorTests()
+        {
+            sut = new AmountCalculator();
+        }
+
+        [MemberData(nameof(ItemsMemberData))]
+        [Theory]
+        public void WhenCalculateIsCalled_ItReturnsCorrectResult(Dictionary<string, int> items, int expectedPrice)
+        {
+            // Act
+            var result = sut.Calculate(items);
+
+            // Assert
+            result.Should().Be(expectedPrice);
+        }
+
+        public static IEnumerable<object[]> ItemsMemberData =>
+            new List<object[]>
+            {
+                    new object[] { new Dictionary<string, int>{ {"A", 3} }, 200 },
+            };
+    }
+}
---
+++ PromotionEngine/Domain/AmountCalculator.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace PromotionEngine.Domain
+{
+    public class AmountCalculator
+    {
+       public int Calculate(Dictionary<string, int> items)
+        {
+            return 0;
+        }
+    }
+}
